f2a64d92dbee03f79321c20b73747590fb3e12ac
t_someya <t_someya@wan55.co.jp>
2025-03-02 12:08:42 +0900

    課題1の実装-02

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import routes from './routes'
 import './index.css'
+import App from './App'
 
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={routes} />
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
--- src/pages/TopPage.jsx
+++ src/pages/TopPage.jsx
@@ -4,11 +4,7 @@ import { posts } from "../data/posts"
 
 const handleAt = createdAt => {
   const date = new Date(createdAt)
-  return {
-    year: date.getFullYear(),
-    month: date.getMonth() + 1,
-    day: date.getDate()
-  }
+  return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
 }
 
 export default function TopPage() {
@@ -19,23 +15,14 @@ export default function TopPage() {
           <div key={post.id} className='mt-10 p-4 border border-gray-300 border-solid'>
             <div className="flex items-center justify-between">
               <time datetime={post.createdAt.split('T')[0]} className="text-gray-500 text-sm">
-                {handleAt(post.createdAt).year}/
-                {handleAt(post.createdAt).month}/
-                {handleAt(post.createdAt).day}
+                {handleAt(post.createdAt)}
               </time>
               <p>
-                {post.categories.map(category => <span className="ml-1 p-1 text-sm text-blue-600 border border-solid border-blue-600 rounded">{category}</span>)}
+                {post.categories.map(category => <span key={category} className="ml-1 p-1 text-sm text-blue-600 border border-solid border-blue-600 rounded">{category}</span>)}
               </p>
             </div>
             <h2 className='text-2xl mt-6 font-medium'>{post.title}</h2>
-            <p className='line-clamp-2 mt-6'>
-              {post.content.split('<br/>').map((content, index) => (
-                <React.Fragment key={index}>
-                {content}
-                <br />
-                </React.Fragment>
-              ))}
-            </p>
+            <p className='line-clamp-2 mt-6' dangerouslySetInnerHTML={{ __html: post.content}} />
           </div>
         ))}
       </div>
